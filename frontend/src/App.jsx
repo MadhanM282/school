b3721157db073fe3c96d29ed2e4d151d717514e7
MadhanM282 <madhanm282@gmail.com>
2022-04-17 18:26:52 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,20 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { Route, Router, Routes } from 'react-router'
 import './App.css'
-
+import { Classes } from './components/Classes'
+import { Login } from './components/Login'
+import ButtonAppBar from './components/nav'
+import { Schools } from './components/School'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <ButtonAppBar/>
+      <Routes>
+        <Route path={"/login"} element={<Login/>} />
+        <Route path={"/"} element={<Schools/>} />
+        <Route path={"/class"} element={<Classes/>} />
+      </Routes>
+      {/* <Schools/> */}
     </div>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { Store } from './Redux/Store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={Store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 )
